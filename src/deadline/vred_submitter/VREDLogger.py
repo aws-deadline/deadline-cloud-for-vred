2c61812c5d479d607ea234febb8caee7ac36f12f
David Israel <isredavi@amazon.com>
2025-04-18 10:09:04 -0700

    feat!: basic global logger for the VRED submitter with console and file-based logging capabilities. Addresses minor changes to spacing, pydocs formatting.
    Signed-off-by: David Israel <isredavi@amazon.com>

--- src/deadline/vred_submitter/VREDLogger.py
+++ src/deadline/vred_submitter/VREDLogger.py
@@ -52,13 +52,19 @@ class VREDLogger(logging.Logger):
         self.propagate = False
 
     def _setup_console_handler(self) -> None:
-        """Configure and add console handler to the logger."""
+        """
+        Configures and adds console handler to the logger.
+        :return: None
+        """
         console_handler = VREDConsoleHandler()
         console_handler.setFormatter(logging.Formatter(self.CONSOLE_LOG_FORMAT))
         self.addHandler(console_handler)
 
     def _setup_file_handler(self) -> None:
-        """Configure and add file handler to the logger."""
+        """
+        Configures and adds file handler to the logger.
+        :return: None
+        """
         log_file = self._get_log_file_path()
         disk_handler = logging.handlers.RotatingFileHandler(
             log_file,
@@ -72,7 +78,6 @@ class VREDLogger(logging.Logger):
         """
         Determine the appropriate log file path. If the default path lacks sufficient
         permission, then attempt to use an alternative temporary path.
-
         :return: path to the log file
         """
         log_file = os.path.expanduser(self.DEFAULT_LOGFILE_LOCATION)
@@ -81,7 +86,6 @@ class VREDLogger(logging.Logger):
                 os.makedirs(os.path.dirname(log_file))
             except (IOError, OSError):
                 return os.path.join(tempfile.gettempdir(), self.ALTERNATIVE_LOGGING_FILENAME)
-
         if not os.access(os.path.dirname(log_file), os.W_OK | os.R_OK):
             return os.path.join(tempfile.gettempdir(), self.ALTERNATIVE_LOGGING_FILENAME)
         return log_file
